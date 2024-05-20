1b62279fcf0a164cdc6dc8809dabd57138db7856
Akash <akkyjat601@gmail.com>
2024-05-20 18:25:37 +0530

    minor bug

--- src/features/product/product.controller.js
+++ src/features/product/product.controller.js
@@ -1,32 +1,31 @@
 import ProductModel from "./product.model.js";
 
-export default class ProductController{
-
-    getAllProducts(req,res){
-        const products = ProductModel.GetAll();
+export default class ProductController {
+    getAllProducts(req, res) {
+        const products = ProductModel.getAll();
         res.status(200).send(products);
     }
 
-    addProduct(req, res){
-        const {name, price, sizes} = req.body;
+    addProduct(req, res) {
+        const { name, price, sizes } = req.body;
         const newProduct = {
             name,
-            price:parseFloat(price),
-            sizes:sizes.split(','),
+            price: parseFloat(price),
+            sizes: sizes.split(","),
             imageUrl: req.file.filename,
         };
-        const createdRecord=ProductModel.add(newProduct);
+        const createdRecord = ProductModel.add(newProduct);
         res.status(201).send(createdRecord);
     }
 
-    rateProduct(req,res){}
+    rateProduct(req, res) {}
 
-    getOneProduct(req,res){
+    getOneProduct(req, res) {
         const id = req.params.id;
         const product = ProductModel.get(id);
-        if(!product){
-            res.status(404).send('Product not found');
-        } else{
+        if (!product) {
+            res.status(404).send("Product not found");
+        } else {
             return res.status(200).send(product);
         }
     }
@@ -35,12 +34,7 @@ export default class ProductController{
         const minPrice = req.query.minPrice;
         const maxPrice = req.query.maxPrice;
         const category = req.query.category;
-        const result = ProductModel.filter(
-            minPrice,
-            maxPrice,
-            category
-        );
+        const result = ProductModel.filter(minPrice, maxPrice, category);
         res.status(200).send(result);
     }
-
 }
